49757c33d6a61f2bf2da5618f0a7b31c4022e01a
isabellaCE <isabellaemiliano14@gmail.com>
2025-05-02 00:58:06 -0300

    chore: removendo comentário

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,7 +2,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
-import '@mdi/font/css/materialdesignicons.min.css' // ESSA LINHA É CRUCIAL
+import '@mdi/font/css/materialdesignicons.min.css'
 
 export default createVuetify({
   components,
