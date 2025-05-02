3f38dce5409203f5019e1ca4a294d2073bdd424a
isabellaCE <isabellaemiliano14@gmail.com>
2025-05-02 00:57:40 -0300

    chore: instalando vuetify, scss

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const drawer = ref(false)
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <v-main style="min-height: 100vh; background: white; padding: 0">
+      <div class="content-container">
+        <div style="padding: 20px">
+          <h2 class="mb-4">Life Events</h2>
+          <v-fab color="#137073" icon="mdi-plus" @click.stop="drawer = !drawer"></v-fab>
+        </div>
+      </div>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        temporary
+        location="right"
+        width="300"
+      >
+        <div>TESTE</div>
+      </v-navigation-drawer>
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.content-container {
+  position: relative;
+  height: 100%;
+  width: 100vw;
+  overflow-x: hidden;
+  background: white;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.v-navigation-drawer {
+  transition: transform 0.3s ease !important;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.v-application {
+  background: white !important;
 }
 </style>
